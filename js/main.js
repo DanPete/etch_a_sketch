eeef4727e93ab99a4384b4033e4df11ed17a19f3
Dan Sack <dsack7@gmail.com>
2016-11-22 22:01:43 +0100

    Change css and js

--- js/main.js
+++ js/main.js
@@ -25,14 +25,14 @@ $(document).ready(function() {
 });
 */
 
-function createGrid(x) {
-  for (var rows = 0; rows < x; rows++) {
-    for (var columns = 0; columns < x; columns++) {
+function createGrid(size) {
+  for (var rows = 0; rows < size; rows++) {
+    for (var columns = 0; columns < size; columns++) {
       $("#container").append("<div class='grid'></div>");
     };
     };
-    $(".grid").width(720/x);
-    $(".grid").height(720/x);
+    $(".grid").width(720/size);
+    $(".grid").height(720/size);
 };
 
 function black() {
@@ -68,33 +68,55 @@ function getRandomClass (){
 
 
 $(document).ready(function() {
-    createGrid(16);
+    size = 16
+    createGrid(size);
     black();
-    $(".reset").click(function (){
-      $("#container").html("");
-      var input = prompt("How big do you want the grid?", "16");
+
+    $('.resize').click(function(){
+      haveSize = false;
+      while (haveSize === false){
+        oldSize = size;
+        size = prompt("How big do you want the grid? (1 -100)", "16");
+        if (size > 0 && size <= 100){haveSize = true}
+        else if(size === null){
+          size = oldSize;
+          haveSize = true  }
+        else {alert("The number you entered is outside the range!")};
+    };
+     $("#container").empty();
+    createGrid(size);
+    black ();
+  });
+
+/*
+      var input = prompt("How big do you want the grid? (1-100)", "16");
       createGrid(parseInt(input));
+      black();
+    });
+*/
+
+
+    $(".classic").click(function (){
+      $("#container").empty();
+      createGrid(size);
       black();
     });
 
     $(".random").click(function (){
-      $("#container").html("");
-      var input = prompt('How big do you want the grid');
-      createGrid(parseInt(input));
+      $("#container").empty();
+      createGrid(size);
       randomColor();
     });
 
     $(".xrandom").click(function (){
-      $("#container").html("");
-      var input = prompt('How big do you want the grid');
-      createGrid(parseInt(input));
+      $("#container").empty();
+      createGrid(size);
       superRandomColor();
     });
 
     $(".shades").click(function (){
-      $("#container").html("");
-      var input = prompt('How big do you want the grid');
-      createGrid(parseInt(input));
+      $("#container").empty();
+      createGrid(size);
       $('.grid').css("background-color", "black");
       $('.grid').css("outline", "none");
       $('.grid').css("opacity", 0);
@@ -104,3 +126,21 @@ $(document).ready(function() {
 
 
 });
+
+
+/*
+
+    $(".resize").click(function (){
+      haveSize = false;
+      while (haveSize === false){
+        oldSize = size;
+        size = prompt("How big do you want the grid? (1-100)", "16");
+        if (size > 0 && <=128){haveSize = true}
+        else if(size === null){
+          size = oldSize;
+          haveSize = true }
+        else {alert("The grid is too large!")};
+        };
+      });
+
+      */
